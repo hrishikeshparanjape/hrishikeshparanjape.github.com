0dbc0b4f3800a1edf52a1e25bfa476a1919cf515
Hrishikesh Paranjape <hrishikeshp19@gmail.com>
2013-01-04 21:40:26 -0800

    Heather Puppies

---
+++ js/index.js
@@ -0,0 +1,107 @@
+App = new Ext.Application({
+	name: "App",
+	launch: function(){
+		var stopCarousel=false;
+		var carousel1 = new Ext.Carousel({
+    		items: [{
+				html:'<img class="imageFit" src="images/britbull.jpg" />'
+    		},{
+				html:'<img class="imageFit" src="images/lab.jpg" />'
+    		},{
+				html:'<img class="imageFit" src="images/ridgeback.jpg" />'
+    		}],
+			listeners:{
+				cardswitch: function(){
+					if (!stopCarousel) this.setActiveItem((this.getActiveIndex() + 1) % 3);
+				
+				}
+			},
+			cardSwitchAnimation:{
+				type:'slide',
+				duration:200,
+				direction:'down'
+			},
+			activeItem:0
+		});
+		var carousel2 = new Ext.Carousel({
+    		items: [{
+				html:'<img class="imageFit" src="images/britbull.jpg" />'
+    		},{
+				html:'<img class="imageFit" src="images/lab.jpg" />'
+    		},{
+				html:'<img class="imageFit" src="images/ridgeback.jpg" />'
+    		}],
+			listeners:{
+				cardswitch:function(){
+					if (!stopCarousel) this.setActiveItem((this.getActiveIndex() + 1) % 3);
+				}
+			},
+			cardSwitchAnimation:{
+				type:'slide',
+				duration:400,
+				direction:'down'
+			},
+			activeItem:1
+		});
+		var carousel3 = new Ext.Carousel({
+    		items: [{
+				html:'<img class="imageFit" src="images/britbull.jpg" />'
+    		},{
+				html:'<img class="imageFit" src="images/lab.jpg" />'
+    		},{
+				html:'<img class="imageFit" src="images/ridgeback.jpg" />'
+    		}],
+			listeners:{
+				cardswitch:function(){
+					if (!stopCarousel) this.setActiveItem((this.getActiveIndex() + 1) % 3);
+				}
+			},
+			cardSwitchAnimation:{
+				type:'slide',
+				duration:300,
+				direction:'down'
+			},
+			activeItem:2
+		});
+		var delayedTask=new Ext.util.DelayedTask(function(){
+			stopCarousel=true;
+			if(carousel1.getActiveIndex()==carousel2.getActiveIndex()&&carousel2.getActiveIndex()==carousel3.getActiveIndex())
+				Ext.Msg.alert('Yeyyy','You got '+ ["English Bulldog", "Lab", "Ridgeback"][carousel1.getActiveIndex()],Ext.emptyFn);
+		});
+		var cmp= new Ext.Panel({
+			layout:{
+				type:'hbox',
+				align:'stretch'
+			},
+			defaults:{
+				flex:1
+			},
+			dockedItems: [{
+				xtype: "toolbar",
+				dock: "top",
+				title:'Heather\'s Puppies',
+				items:[{
+					xtype:'button',
+					text:'Hit',
+					handler:function(){
+							stopCarousel=false;
+							carousel1.setActiveItem((carousel1.getActiveIndex()+1)%3);
+							carousel2.setActiveItem((carousel2.getActiveIndex()+1)%3);
+							carousel3.setActiveItem((carousel3.getActiveIndex()+1)%3);
+							delayedTask.delay(2000);	
+					}
+				}]
+			}],
+			items:[carousel1,carousel2,carousel3]
+		});
+		this.viewport = new Ext.Panel({
+		fullscreen: true,
+		layout:'fit',
+		items:cmp
+		});
+		
+	}
+	
+});
+//end of module
+
